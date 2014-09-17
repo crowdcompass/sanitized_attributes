2dda6e4a05f2d93c53845259e96dddff9ea8455c
Laurie Kemmerer <lkemmerer@crowdcompass.com>
2014-09-16 17:56:35 -0700

    Update sanitize gem
    I’m not positive that all the spec changes are acceptable, as to some
      extent, I’m not sure how much was relying on the default behavior of
      the old sanitize version vs. what’s actually cared about.

--- lib/sanitized_attributes/sanitized_attribute.rb
+++ lib/sanitized_attributes/sanitized_attribute.rb
@@ -7,7 +7,9 @@ module SanitizedAttributes; class SanitizedAttribute
 
   def sanitize(content)
     if content
-      Sanitize.clean("<SPURIOUS-TOPLEVEL>" + content + "</SPURIOUS-TOPLEVEL>", sanitize_config).gsub(%r{</?SPURIOUS-TOPLEVEL>}, "")
+      Sanitize.fragment(content, sanitize_config).
+        # equivalent of squish, which Sanitize used to do
+        gsub(/\s+/, ' ').sub(/^\s/, '').sub(/\s$/, '')
     end
   end
 
